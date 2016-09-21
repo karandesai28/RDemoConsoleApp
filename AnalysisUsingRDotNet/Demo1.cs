35e3a0dfade804616bca84a0c6215a2d8e3ea7f9
Karan Desai <karan.desai@promactinfo.com>
2016-09-21 19:40:31 +0530

    Contextual analysis-Quartz scheduler implementation

--- AnalysisUsingRDotNet/Demo1.cs
+++ AnalysisUsingRDotNet/Demo1.cs
@@ -1,11 +1,50 @@
 ﻿using System;
 using RDotNet;
+using Quartz;
+using Quartz.Impl;
 
 namespace RCalculator
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            // construct a scheduler factory
+            ISchedulerFactory schedFact = new StdSchedulerFactory();
+
+            // get a scheduler
+            IScheduler sched = schedFact.GetScheduler();
+            
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<Forecast>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+           
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+              .WithIdentity("myTrigger", "group1")
+              .StartNow()
+              //.WithSimpleSchedule(x => x
+              //    .WithIntervalInSeconds(5)
+               //   .WithRepeatCount(5)
+                //  )
+              //.WithDailyTimeIntervalSchedule(x=>x.WithIntervalInHours(24).OnEveryDay().StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(15,55)))                          
+              .Build();
+
+            //Start
+            sched.Start();
+
+            sched.ScheduleJob(job, trigger);
+
+        }
+    }
+
+    class Forecast : IJob
+    {
+        public void Execute(IJobExecutionContext context)
         {
             REngine engine;
 
@@ -19,25 +58,22 @@ namespace RCalculator
             var demomsg = engine.Evaluate("3*4");
             Console.WriteLine(demomsg);
 
-
-
             //A matrix is simply a rectangular array of numbers and a vector is a row (or column) of a matrix.A vector can be considered as 1 by n matrix or n by 1 matrix .
-            NumericMatrix values = engine.Evaluate("data<-c( 4,234,12324234,13557788,2345,243,0,2,567886 )").AsNumericMatrix();
-            engine.Evaluate("datats.ts<-ts(data,frequency=3)");
-            engine.Evaluate("datadecompose<-decompose(datats.ts)");
+            var values = engine.Evaluate("data<-c( 100,134,234,500,342,125,56,435,678,244,876,123,45,46,356)");
             
-            engine.Evaluate("HoltWinters(datats.ts)");
+            engine.Evaluate("datats<-ts(data,start=c(1,1), frequency=7)"); //f=7 for daily. start=c(1,1) for 1st day of 1st week
 
-            
-            //Console.WriteLine(assigntoTS.AsCharacter());
 
-           
+            engine.Evaluate("dataHW<-HoltWinters(datats,gamma=FALSE)");
+            // var package = engine.Evaluate("path<-find.package(\"forecast\")"); //In case library did not load find the path of package explicitly
+            engine.Evaluate("library(\"forecast\")");
+            engine.Evaluate("forecast.HoltWinters(dataHW,h=1)");
+
             Console.ReadLine();
-            
-            
+
+
             //clean up
             engine.Dispose();
-
         }
     }
 }
